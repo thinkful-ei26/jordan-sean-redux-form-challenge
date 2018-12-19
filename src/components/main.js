8bd1414965f3b14a54e1e599596aa964ecd038a1
seankrummel <sean.krummel@gmail.com>
2018-12-19 13:40:32 -0600

    converted form to redux form and started validators

--- src/components/main.js
+++ src/components/main.js
@@ -14,19 +14,29 @@ class Main extends React.Component {
         <form onSubmit={this.props.handleSubmit(values => this.onSubmit(values))}>
             <h1>Report a problem with your delivery</h1>
             <label htmlFor='tracking'>Tracking number required</label>
-            <input id='tracking'/>
+            {/* <input id='tracking'/> */}
+            <Field name='tracking' id='tracking' type='text' component='input' />
             <div className = 'issues'>
                 <label htmlFor='issue-selection'>What is your issue?</label>
-                <select id='issue-selection'>
+                {/* <select id='issue-selection'>
                     <option value='My delivery hasnt arrived'>My delivery hasnt arrived</option>
                     <option value='The wrong item was delivered'>The wrong item was delivered</option>
                     <option value='Part of my order was missing'>Part of my order was missing</option>
                     <option value='Some of my order arrived damaged'>Some of my order arrived damaged</option>
                     <option value='Other (Please provide details below)'>Other (Please provide details below)</option>
-                </select>
+                </select> */}
+                <Field name='issue-selection' id='issue-selection' component='select'>
+                    <option value=''>-- Please Select --</option>
+                    <option value='My delivery hasnt arrived'>My delivery hasnt arrived</option>
+                    <option value='The wrong item was delivered'>The wrong item was delivered</option>
+                    <option value='Part of my order was missing'>Part of my order was missing</option>
+                    <option value='Some of my order arrived damaged'>Some of my order arrived damaged</option>
+                    <option value='Other (Please provide details below)'>Other (Please provide details below)</option>
+                </Field>
                 <div>
                     <label htmlFor='issue-detail'>Give more details (optional)</label>
-                    <textarea id='issue-detail' rows='5' cols='30'></textarea>
+                    {/* <textarea id='issue-detail' rows='5' cols='30'></textarea> */}
+                    <Field name='issue-detail' id='issue-detail' rows='5' cols='30' component='textarea' />
                 </div>
             </div>
             <input type='submit' value='Submit' />
@@ -34,4 +44,4 @@ class Main extends React.Component {
     }
 }
 
-export default reduxForm({form: ''})(Main);
+export default reduxForm({form: 'deliveryForm'})(Main);
---
+++ src/validators.js
@@ -0,0 +1,12 @@
+// Validation Requirements:
+// Contains a value
+// The value is non-empty
+// The value is be exactly 5 characters long
+// Each character is a number
+
+export const containsValue = value => value ? undefined : 'Must contain a value';
+// export const nonEmpty = value => value === '' ? 'The value must not be empty' : undefined;
+export const fiveCharacters = value => value.length === 5 ? undefined : 'The value must be 5 characters long';
+export const numbers = value => Number(value) === NaN ? 'Each character must be a number' : undefined;
+
+// bool expression ? operation if true : operation if false
